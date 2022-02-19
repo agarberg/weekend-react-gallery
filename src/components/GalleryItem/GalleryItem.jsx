83e6e460edadd58c53464b278a80007c35fa9244
Adam Garberg <adamgarberg@gmail.com>
2022-02-19 12:19:24 -0600

    image_desc toggler functional

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,21 +1,29 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './GalleryItem.css'
 
 
 function GalleryItem({item}) {
-console.log(item)
+    // const showImage = item.path;
+    // const showDescription = item.description;
+
+    const [image, setDescription] = useState(true);
+    const toggler = () => {
+        console.log('in Toggler');
+        setDescription(!image);
+    }
+    console.log(image)
+    console.log(item)
     return (
         <>
-
-
-    <div>
-        <img src={item.path} />
-        <p>{item.description}</p>
+    <div className="GalleryItem">
+    <div className="PicDesc" onClick={toggler}>
+        {image? <img src={item.path}/>:<p>{item.description}</p>}
     </div>
-    <div>
+    <div className="Likes">
         <p>{item.likes} Likes</p>
         <button>Like Me!</button>
+    </div>
     </div>
     </>
     )
