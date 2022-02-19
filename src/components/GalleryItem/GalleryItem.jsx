5b7dbb4efbe6a84da2d9827ed6d528ee8e75b859
Adam Garberg <adamgarberg@gmail.com>
2022-02-18 20:50:03 -0600

    gallery list and item started

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,12 +1,20 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+
 import './GalleryItem.css'
 
 function GalleryItem ({galleryItems}) {
-
-
     return (
-
+        <>
+    <div>
+        <img src={galleryItems.path} alt={galleryItems.path} />
+        <p>{galleryItems.description}</p>
+    </div>
+    <div>
+        <p>{galleryItems.likes}</p>
+        <button>Like This</button>
+    </div>
+    </>
     )
 }
 
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,17 +1,19 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import GalleryItem from '../GalleryItem/GalleryItem'
 import './GalleryList.css'
 
 
 function RenderGallery({galleryItems}) {
 
     return (
-        <div class="gallery">  </div>
+        <div class="gallery"> 
         {galleryItems.map => (
-            galleryItem.path
+            <li key={galleryItems.id}> 
+            <GalleryItem galleryItems={galleryItems} />
+            </li>
         )}
         </div>
-
     )
 }
 
